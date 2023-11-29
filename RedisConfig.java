618b93e88262a6fe66c48686f1ba36c9bcf37b36
shyamkrishna762 <98841211+shyamkrishna762@users.noreply.github.com>
2023-11-29 18:09:57 +0530

    Update RedisConfig.java

--- RedisConfig.java
+++ RedisConfig.java
@@ -1,30 +1,36 @@
-import org.springframework.cache.annotation.EnableCaching;
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
+import io.lettuce.core.resource.ClientResources;
+import io.lettuce.core.resource.DefaultClientResources;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.cache.interceptor.KeyGenerator;
-import org.springframework.cache.interceptor.SimpleKey;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
-@EnableCaching
 public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
-        jedisClientConfiguration.useSsl().keyStoreType("PEM").keyResource(new ClassPathResource("path/to/your/certificate.pem"));
-        
-        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(jedisClientConfiguration.build());
-        jedisConnectionFactory.setHostName("your.redis.host");
-        jedisConnectionFactory.setPort(6379);
-        jedisConnectionFactory.setPassword("your_redis_password");
+        ClientOptions clientOptions = ClientOptions.builder()
+                .socketOptions(SocketOptions.builder().ssl(true).build())
+                .build();
 
-        return jedisConnectionFactory;
+        ClientResources clientResources = DefaultClientResources.builder().build();
+
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        lettuceConnectionFactory.setHostName("your.redis.host");
+        lettuceConnectionFactory.setPort(6379);
+        lettuceConnectionFactory.setPassword("your_redis_password");
+        lettuceConnectionFactory.setClientOptions(clientOptions);
+        lettuceConnectionFactory.setClientResources(clientResources);
+        lettuceConnectionFactory.setKeyStoreProvider(new FileKeyStoreProvider(new ClassPathResource("path/to/your/certificate.pem")));
+
+        return lettuceConnectionFactory;
     }
 
     @Bean
