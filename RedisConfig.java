0abdd75476108ed2d1d3a78a8db96d0bc12b4052
shyamkrishna762 <98841211+shyamkrishna762@users.noreply.github.com>
2023-12-01 19:13:19 +0530

    Update RedisConfig.java

--- RedisConfig.java
+++ RedisConfig.java
@@ -1,44 +1,43 @@
-import io.lettuce.core.ClientOptions;
-import io.lettuce.core.SocketOptions;
-import io.lettuce.core.resource.ClientResources;
-import io.lettuce.core.resource.DefaultClientResources;
+package com.smartwealth.transactionservice.config;
+
+import java.time.Duration;
+
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 public class RedisConfig {
 
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        ClientOptions clientOptions = ClientOptions.builder()
-                .socketOptions(SocketOptions.builder().ssl(true).build())
-                .build();
+	@Bean
+	public RedisCacheConfiguration cacheConfiguration() {
+	    return RedisCacheConfiguration.defaultCacheConfig()
+	      .entryTtl(Duration.ofMinutes(60))
+	      .disableCachingNullValues()
+	      .serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+	}
+	
+	@Bean
+	public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+	    return (builder) -> builder
+	      .withCacheConfiguration("InvestorDetailsByPan",
+	        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(10)))
+	      .withCacheConfiguration("customerCache",
+	        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(5)));
+	}
 
-        ClientResources clientResources = DefaultClientResources.builder().build();
-
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
-        lettuceConnectionFactory.setHostName("your.redis.host");
-        lettuceConnectionFactory.setPort(6379);
-        lettuceConnectionFactory.setPassword("your_redis_password");
-        lettuceConnectionFactory.setClientOptions(clientOptions);
-        lettuceConnectionFactory.setClientResources(clientResources);
-        lettuceConnectionFactory.setKeyStoreProvider(new FileKeyStoreProvider(new ClassPathResource("path/to/your/certificate.pem")));
-
-        return lettuceConnectionFactory;
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        return template;
-    }
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+//        RedisTemplate<String, Object> template = new RedisTemplate<>();
+//        template.setConnectionFactory(connectionFactory);
+//        template.setKeySerializer(new StringRedisSerializer());
+//        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+//        return template;
+//    }
 }
